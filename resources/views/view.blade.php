4e6f66ec638a7371e6fadcf828ce58f9121d6a4c
Iz <me@izdiwho.com>
2023-03-11 16:27:09 +0800

    fix sxss

--- resources/views/view.blade.php
+++ resources/views/view.blade.php
@@ -3,8 +3,7 @@
 
 @section('content')
     <h2>{{ $note->title }}</h2>
-    <p>{!! $note->body !!}</p>
-    {{-- <p>{{ $note->body }}</p> --}}
+    <p>{{ $note->body }}</p>
     <p>
         <a href="/notes/{{ $note->id }}/delete">Delete</a>
     </p>
